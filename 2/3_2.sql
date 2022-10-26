625c4834892e63e2d7371b36bb29e5e2ffd63cae
ThompsonA93 <thauer@edu.aau.at>
2022-10-26 22:26:19 +0200

    Hotfix of overwrite 3.2 with output

--- 2/3_2.sql
+++ 2/3_2.sql
@@ -1,3 +1,21 @@
-"""Villach""","""Scholz Ben"""
-"""CSI Miami""","""Hoffmann Emily"""
-"""Klagenfurt""","""Gro� Sara"""
+/**
+Write a query querying myFiles, that returns the title and owner name of all groups,
+where Hanna Schmidt is a member. The result should be a standard relation (no JSON).
+  **/
+
+/* For nesting, check 'Addressing JSON in SQL'
+-> :: Get key+value
+->> :: Get value
+#> :: On arrays, get key+value
+#>> :: On arrays, get key
+https://www.postgresql.org/docs/9.4/functions-json.html
+jsonb_array_elements(jsonb): Expands a JSON array to a set of JSON values.
+*/
+
+SELECT grp.groups->'title' AS title, grp.groups->'owner' AS owner -- select groups and owners
+FROM (
+    SELECT jsonb_array_elements(content->'groups') AS groups FROM myfiles
+) AS grp -- unnest groups to group
+WHERE 'Schmidt Hanna' IN (
+    SELECT jsonb_array_elements(grp.groups->'members')->>'name'
+); -- unnest group to member
