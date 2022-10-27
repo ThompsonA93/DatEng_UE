6e45c3b70dcf23c383366eedefff0f1d6681072d
ThompsonA93 <thauer@edu.aau.at>
2022-10-27 10:10:35 +0200

    Working 5.2

--- 2/5_2.sql
+++ 2/5_2.sql
@@ -3,11 +3,40 @@ Create a query that returns the total number of people employed in each
 department including all employees of its sub-departments.
 **/
 
+-- Only show sum for one given department
+WITH RECURSIVE employeecount(deptId, name, parentId, numEmpl) AS (
+        SELECT d."deptId", name, "parentId", "numEmpl"
+        FROM department d
+        WHERE "deptId"=2
+        UNION
+            SELECT dd."deptId", dd.name, dd."parentId", dd."numEmpl"
+            FROM department dd, employeecount ec
+            WHERE dd."parentId"=ec.deptId
+    ) SELECT sum(employeecount.numEmpl) FROM employeecount;
+
+-- WORKING Example with CROSS JOIN LATERAL
+-- Lateral required for reference of inner recursive function to outerdept
+SELECT * FROM department outerDept CROSS JOIN LATERAL(
+    WITH RECURSIVE department_children("deptId", "numEmpl") AS (
+        SELECT "deptId", "numEmpl"
+        FROM department d1
+        WHERE d1."deptId" = outerDept."deptId"
+        UNION
+            SELECT nextDept."deptId", nextDept."numEmpl"
+            FROM department_children lastDept, department nextDept
+            WHERE nextDept."parentId" = lastDept."deptId"
+    )
+    select SUM("numEmpl") as totalEmployees from department_children
+) innerSum;
+
+
+
 -- Test: Get total sum of all employees over all departments
 WITH RECURSIVE employeecount(deptId, name, parentId, numEmpl) AS
     (
         SELECT d."deptId", name, "parentId", "numEmpl"
         FROM department d
+        WHERE d."deptId"=1
         UNION
             SELECT dd."deptId", dd.name, dd."parentId", dd."numEmpl"
             FROM department dd, employeecount ec
