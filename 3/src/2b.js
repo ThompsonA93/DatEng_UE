abf3f5e1015596ca1f3e544b70fde5b804ef4181
ThompsonA93 <thauer@edu.aau.at>
2022-11-09 12:48:10 +0100

    Needs fix on 2c

--- 3/src/2b.js
+++ 3/src/2b.js
@@ -1,1 +1,24 @@
-// Rename the director Roland Emmerich to R. Emmerich. Be sure to update all his movies.
+// 2b) Rename the director Roland Emmerich to R. Emmerich. Be sure to update all his movies.
+
+// 3 Movies
+db.video_movieDetails.find(
+    {director: "Roland Emmerich"},
+    {_id: 0, title: 1, director: 1}
+)
+
+// Update Director name
+db.video_movieDetails.updateMany(
+    {director: "Roland Emmerich"},
+    {$set: {director: "R. Emmerich"}}
+)
+
+db.video_movieDetails.find(
+    {director: "R. Emmerich"},
+    {_id: 0, title: 1, director: 1}
+)
+
+// Revert update director name for later showcasing
+db.video_movieDetails.updateMany(
+    {director: "R. Emmerich"},
+    {$set: {director: "Roland Emmerich"}}
+)
--- 3/src/2c.js
+++ 3/src/2c.js
@@ -1,2 +1,22 @@
-// Return all movie titles (video_movies.title) of all movies that were filmed in France (video_movieDetails.country).
+// Return all movie titles (video_movies.title) of all movies that were filmed in
+// France (video_movieDetails.country).
 // The lookup function might be useful: https://docs.mongodb.com/manual/reference/operator/aggregation/lookup/
+
+use video;
+
+// ???
+db.video_movieDetails.find({countries: "France"})
+
+// FIXME
+db.video_movies([{
+    $lookup: {
+        from: "video_movieDetails",
+        let: {
+            movie_title: "$title", movie_country: "$country"
+        },
+        pipeline: [{
+            $match: { $country: "France"}
+        }]
+    }
+}])
+
