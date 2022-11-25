d5856e75070b6cc61a3e5a82d633b8dddfceb66c
ThompsonA93 <thauer@edu.aau.at>
2022-11-25 15:07:34 +0100

    Base seleections running, but needs more layers which is problematic

--- 4/sql/star_schema/1a_1_DWHCreation.sql
+++ 4/sql/star_schema/1a_1_DWHCreation.sql
@@ -8,7 +8,7 @@ DROP TABLE IF EXISTS Studyplan;
 -- {Name, Rank (Univ Ass, Postdoc-Ass, Prof, Ass Prof, …), Title (DI, DR,…)} → Department → University
 CREATE TABLE Lecturer(
     LecturerID INT NOT NULL,
-    Name VARCHAR(255) NOT NULL,
+    "Name" VARCHAR(255) NOT NULL,
     Rank VARCHAR(255) NOT NULL,
     Title VARCHAR(255) NOT NULL,
     Department VARCHAR(255) NOT NULL,
@@ -24,9 +24,9 @@ CREATE TABLE Lecturer(
 CREATE TABLE Course(
     CourseID VARCHAR(255) NOT NULL, -- "ID listed as XXX.XXX" :: Assuming Varchar due to '.'
     Course VARCHAR(255) NOT NULL,
-    Type VARCHAR(255) NOT NULL,
+    "Type" VARCHAR(255) NOT NULL,
     ECTS INT NOT NULL,
-    Level VARCHAR(255) NOT NULL,
+    "Level" VARCHAR(255) NOT NULL,
     Department VARCHAR(255) NOT NULL,
     UniversityName VARCHAR(255) NOT NULL,
     CONSTRAINT PK_Course PRIMARY KEY (
@@ -37,23 +37,23 @@ CREATE TABLE Course(
 -- Day → Month → Semester → Year
 CREATE TABLE Time(
     TimeID SERIAL NOT NULL, -- Serial: Autogenerate key, as we don't have an ID written in JSON
-    Day INT NOT NULL,
-    Month INT NOT NULL,
+    "Day" INT NOT NULL,
+    "Month" INT NOT NULL,
     Semester VARCHAR(255), -- No entry matching semester :: Assuming SS/WS
-    Year INT NOT NULL,
+    "Year" INT NOT NULL,
     CONSTRAINT Pk_Time PRIMARY KEY(
         TimeID
     )
 )
 -- Access each dimensionality of Time (D, M, Y) -> Index
 -- https://dba.stackexchange.com/questions/31420/how-to-create-unique-index-for-month-and-year-column
-CREATE UNIQUE INDEX ON Time(Day, Month, Year);
+CREATE UNIQUE INDEX ON Time("Day", "Month", "Year");
 
 
 -- Name
 CREATE TABLE Student(
     StudentID INT NOT NULL, -- 9,xxx,xxx < 2,147,483,647 (Max INT)
-    Name VARCHAR(255),
+    "Name" VARCHAR(255),
     CONSTRAINT PK_STUDENT PRIMARY KEY(
         StudentID
     )
--- 4/sql/star_schema/1b_2_JSONtoStarSchema.sql
+++ 4/sql/star_schema/1b_2_JSONtoStarSchema.sql
@@ -1,3 +1,37 @@
 /** 1b_2_JSONtoStarSchema.sql **/
 
-SELECT data->>'master' FROM json_aau_corses;
+
+/** Required Fields: **/
+-- Corses: courseid, course, type, ects, level, department,
+-- Metadata: universityname
+
+
+-- Selection from metadata
+SELECT
+    aau_metadata.data->'name'
+FROM json_aau_metadata as aau_metadata;
+
+-- Selection from master corses
+SELECT
+    master_data->>'id' id,
+    master_data->>'title' title,
+    master_data->>'type' "type",
+    master_data->>'lecturer' lecturer,
+    master_data->>'ECTS' ects,
+    master_data->>'department' department
+FROM json_aau_corses as masters,
+    jsonb_array_elements(masters.data->'master') master_data;
+
+-- Select Levels for each course
+SELECT
+    jsonb_object_keys(corses.data) as "Level"
+FROM json_aau_corses as corses;
+
+-- Merge previous statements and insert into course table
+-- INSERT INTO course(courseid, course, "Type", ects, "Level", department, universityname)
+-- FIXME
+SELECT
+    jsonb_object_keys(courses.data)
+FROM json_aau_corses as courses,
+    jsonb_array_elements(courses.data) course_data
+;
