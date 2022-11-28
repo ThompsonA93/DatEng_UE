3b97da35ac4e35fba93c38a607eb4aa82daea72c
ThompsonA93 <thauer@edu.aau.at>
2022-11-28 17:16:45 +0100

    Fix First query

--- 4/sql/star_schema/1b_2_JSONtoStarSchema.sql
+++ 4/sql/star_schema/1b_2_JSONtoStarSchema.sql
@@ -1,15 +1,13 @@
 /** 1b_2_JSONtoStarSchema.sql **/
-
-
 /** Required Fields: **/
 -- Corses: courseid, course, type, ects, level, department,
 -- Metadata: universityname
-
 
 -- Selection from metadata
 SELECT
     aau_metadata.data->'name'
 FROM json_aau_metadata as aau_metadata;
+
 
 -- Selection from master corses
 SELECT
@@ -22,16 +20,28 @@ SELECT
 FROM json_aau_corses as masters,
     jsonb_array_elements(masters.data->'master') master_data;
 
+
 -- Select Levels for each course
 SELECT
     jsonb_object_keys(corses.data) as "Level"
 FROM json_aau_corses as corses;
 
+
 -- Merge previous statements and insert into course table
 -- INSERT INTO course(courseid, course, "Type", ects, "Level", department, universityname)
--- FIXME
+-- Select Levels for each course
 SELECT
-    jsonb_object_keys(courses.data)
-FROM json_aau_corses as courses,
-    jsonb_array_elements(courses.data) course_data
-;
+    master_data->>'id' id,
+    master_data->>'title' title,
+    master_data->>'type' "type",
+    master_data->>'ECTS' ects,
+    jsonb_object_keys(masters.data) "level",
+    master_data->>'department' department,
+    metadata.data->'name' "universityname"
+FROM json_aau_corses as masters,
+     json_aau_metadata as metadata,
+    jsonb_array_elements(masters.data->'master') master_data;
+
+
+
+
